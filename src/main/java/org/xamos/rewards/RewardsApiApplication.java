05161c95af6d2c64d9387535fb1bc32f1ba041a8
IkenoXamos <moberlies1337@gmail.com>
2025-04-30 15:37:48 -0700

    Initial Commit

---
+++ src/main/java/org/xamos/rewards/RewardsApiApplication.java
@@ -0,0 +1,13 @@
+package org.xamos.rewards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RewardsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/xamos/rewards/RewardsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xamos.rewards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
