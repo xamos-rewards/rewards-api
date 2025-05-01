b1553cbc1406842432d3ac051c8deb389351c9c5
IkenoXamos <moberlies1337@gmail.com>
2025-04-30 23:39:20 -0700

    Reformat tabs to spaces

--- src/main/java/org/xamos/rewards/RewardsApiApplication.java
+++ src/main/java/org/xamos/rewards/RewardsApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RewardsApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RewardsApiApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(RewardsApiApplication.class, args);
+  }
 
 }
--- src/test/java/org/xamos/rewards/RewardsApiApplicationTests.java
+++ src/test/java/org/xamos/rewards/RewardsApiApplicationTests.java
@@ -8,8 +8,8 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles({"unit-test"})
 class RewardsApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
