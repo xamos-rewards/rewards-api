33dbd6d4a3ebae1be2457bf22cece89bb4a59672
IkenoXamos <moberlies1337@gmail.com>
2025-04-30 23:36:21 -0700

    Create initial application configuration structure

--- src/test/java/org/xamos/rewards/RewardsApiApplicationTests.java
+++ src/test/java/org/xamos/rewards/RewardsApiApplicationTests.java
@@ -2,8 +2,10 @@ package org.xamos.rewards;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles({"unit-test"})
 class RewardsApiApplicationTests {
 
 	@Test
