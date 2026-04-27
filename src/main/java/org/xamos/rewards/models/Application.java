8a455f7be0253bb007905b406466856f5557f98f
IkenoXamos <moberlies1337@gmail.com>
2026-04-26 17:47:31 -0700

    Update applications table and entity with isActive and ownerId

--- src/main/java/org/xamos/rewards/models/Application.java
+++ src/main/java/org/xamos/rewards/models/Application.java
@@ -1,9 +1,6 @@
 package org.xamos.rewards.models;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,10 +13,17 @@ import lombok.NoArgsConstructor;
 public class Application {
 
   @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   private String name;
 
   @Column(name = "client_id")
   private String clientId;
+
+  @Column(name = "is_active")
+  private boolean isActive;
+
+  @Column(name = "owner_id")
+  private String ownerId;
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,5 @@
-INSERT INTO applications (name, client_id) VALUES ('test-app', 'test-client-id');
-
 INSERT INTO rewards (username, points) VALUES ('auth0|1234567890', 100);
+INSERT INTO rewards (username, points) VALUES ('auth0|0987654321', 250);
+
+INSERT INTO applications (name, client_id, is_active, owner_id) 
+VALUES ('test-app', 'test-client-id', TRUE, 'auth0|1234567890');
